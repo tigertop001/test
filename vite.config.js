b18d20b37c542e3f40f87ffd0139213e3bf3838d
tigertop001 <78149089+tigertop001@users.noreply.github.com>
2023-04-23 18:14:01 +0400

    test

---
+++ vite.config.js
@@ -0,0 +1,40 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import styleImport, { VantResolve } from 'vite-plugin-style-import'
+import postCssPxToRem from 'postcss-pxtorem'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      assets: '@/assets',
+      utils: '@/utils',
+      api: '@/api'
+    }
+  },
+  plugins: [
+    vue(),
+    styleImport({
+      resolves: [VantResolve()]
+    })
+  ],
+  // Vite自身已经集成PostCSS，无需再次安装。另外也无需单独创建PostCSS配置文件，已集成到vite.config.js的css选项中
+  css: {
+    postcss: {
+      plugins: [
+        postCssPxToRem({
+          rootValue: 37.5,
+          propList: ['*']
+        })
+      ]
+    }
+  },
+  server: {
+    proxy: {
+      '/v1': {
+        target: 'http://localhost:8001'
+      }
+    }
+  }
+})
